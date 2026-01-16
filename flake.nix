ee7fb4da0572715f79ec52a67e76ed60e6bf1d39
Agahnim <gabrielguysoulard@gmail.com>
2026-01-16 15:14:13 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake pour dev MkDocs + Material";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python314
+        python314Packages.mkdocs
+        python314Packages.mkdocs-material
+      ];
+
+      shellHook = ''
+        neovide &
+      '';
+    };
+  };
+}
